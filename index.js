fa5c22dce705c6506e0b72b00b7641212157518a
Nahidul Islam <mnisiddique@ael-bd.com>
2020-01-20 14:13:18 +0600

    First commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+//import App from './App';
+import {name as appName} from './app.json';
+import SectionList from './SectionList';
+
+AppRegistry.registerComponent(appName, () => SectionList);
